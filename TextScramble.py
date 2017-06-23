f13614573656246ce88e22f883e0e4d9abe7d56b
daleinen <daleinen@hotmail.com>
2017-06-23 17:23:30 -0500

    Update TextScramble.py

--- TextScramble.py
+++ TextScramble.py
@@ -7,7 +7,7 @@ import time
 
 possibleCharacters = string.ascii_lowercase + string.digits + string.ascii_uppercase + ' .,!?;:'
 
-target = input("\nEnter your target text: ")
+target = input("\n\nEnter your target text: ")
 attemptThis = ''.join(random.choice(possibleCharacters) for i in range(len(target)))
 completed = False
 generation = 0
@@ -30,6 +30,7 @@ while completed == False:
             
     generation += 1
     attemptThis = attemptNext
-    time.sleep(0.1)
+    time.sleep(0.05)
 
+time.sleep(0.5)
 print("\nTarget matched! That took " + str(generation) + " generation(s)\n")
